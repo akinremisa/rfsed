7730f65744459f8115cf5ffc18c3967ea66b6da9
akinremisa <107252532+akinremisa@users.noreply.github.com>
2023-11-09 22:49:01 +0100

    Updated setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-#%% Setup the pip rfsed package
 import setuptools
+from setuptools import setup
 
 with open("./README.md", "r") as fh:
     long_description = fh.read()
-REQUIRES = ['decorator', 'matplotlib>=2', 'numpy', 'scipy',
-             'obspy>=1.0.3', 'tqdm', 'OWSLib',
+REQUIRES = ['decorator', 'matplotlib', 'numpy', 'scipy',
+             'obspy>=1.0.3', 'tqdm', 'OWSLib', 'pytest',
             'cartopy', 'geographiclib', 'shapely', 'seaborn',
             'scipy', 'scikit-learn', 'h5','h5py', 'obspyh5', 
             'colorama==0.1.6', 'plotly==5.17.0', 'psutil==5.9.6']
@@ -14,7 +14,6 @@ CLASSIFIERS = [
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.9',
     'Topic :: Scientific/Engineering :: Physics'
     ]
 EXTRAS_REQUIRE = {
@@ -26,14 +25,15 @@ EXTRAS_REQUIRE = {
 setuptools.setup(
     name="rfsed",
     version="0.0.1",
-    author='Stephen Akinremi, Islam Fadel',
-    author_email=['s.akinremi@utwente.nl', 'i.e.a.m.fadel@utwente.nl'],
+    author='Stephen Akinremi',
+    author_email='s.akinremi@utwente.nl',
     description="Package for receiver functions analysis and dealing with sediment effects in receiver functions",
     long_description=long_description,
     url="https://github.com/akinremisa/rfsed",
     packages=setuptools.find_packages(),
+    setup_requires=REQUIRES,
     install_requires=REQUIRES,
     extras_require=EXTRAS_REQUIRE,
     classifiers=CLASSIFIERS,
-    python_requires='>=3.6',
+    python_requires='>=3.6'
 )
