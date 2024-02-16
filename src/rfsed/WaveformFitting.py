7bd7c0ab041ad5e7fd9c59420a4205037699a512
akinremisa <107252532+akinremisa@users.noreply.github.com>
2024-02-16 10:35:35 +0100

    Editted examples to include preonset parameter

--- src/rfsed/WaveformFitting.py
+++ src/rfsed/WaveformFitting.py
@@ -212,7 +212,7 @@ def PlotWaveformFitting(WaveformFitting, wtCorr, wtRMSE, wtPG, savepath, format)
     #----------------------------------------------------------
     labels=['Correlation Coefficient', 'Root-mean-square Error', 'Phase Goodness of Fit', 'Overall Goodness of Fit']
     model_criteria=zip(Corr, RMSE, PG, GoF)
-    print(model_criteria)
+    # print(model_criteria)
     for i in model_criteria:
         print(i)
     for i in range(len(labels)):
